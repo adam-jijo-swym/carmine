a09cb08d6f39b640f7d60b590a6ed14b82d1ad12
Adam Jijo <158173116+adam-jijo-swym@users.noreply.github.com>
2024-09-25 18:14:27 +0530

    rename project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/carmine "3.4.1-compatibility-SNAPSHOT"
+(defproject com.adam-jijo-swym/swym-carmine "3.4.1-compatibility-SNAPSHOT"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Redis client + message queue for Clojure"
   :url "https://github.com/taoensso/carmine"
